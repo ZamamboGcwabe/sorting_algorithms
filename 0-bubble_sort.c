d2c803a6a4807ac202ae979f7ce7ef67969c629e
thabisile <thabisilemkhize@gmail.com>
2024-03-05 14:38:28 -0800

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,17 +26,17 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-	
+
 	while (bubbly == false)
 	{
 		bubbly = true;
 		for (i = 0; i < len - 1; i++)
 		{
-			if (array[i] > array[i +1])
+			if (array[i] > array[i + 1])
 			{
 				swap(array + i, array + i + 1);
-				print_array(array, size):
-					bubbly = false;
+				print_array(array, size);
+				bubbly = false;
 			}
 		}
 		len--;
