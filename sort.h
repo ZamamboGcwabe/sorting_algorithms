714fc2f8c3ae641f7ca25d70d70648f4f0a4af05
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-03-05 22:26:46 +0000

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,6 +1,9 @@
 #ifndef sort_h
 #define sort_h
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,11 +18,24 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap(int *m, int *n);
+void swap(listint_t **h, listint_t **x, listint_t *y);
 
 #endif 
