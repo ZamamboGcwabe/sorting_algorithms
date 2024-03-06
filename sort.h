452a27d777b958f067383e7e0def0ee2c2f4d55d
thabisile <thabisilemkhize@gmail.com>
2024-03-06 08:09:51 -0800

    Quick sort

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -38,4 +38,4 @@ void quick_sort(int *array, size_t size);
 void swap(int *m, int *n);
 void swap_node(listint_t **h, listint_t **x, listint_t *y);
 
-#endif 
+#endif
